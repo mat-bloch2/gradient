6505e7bc176b1bc2ef657407dce4ffc24d916b58
mat-bloch2 <48055190+mat-bloch2@users.noreply.github.com>
2023-04-21 13:19:00 +0200

    Dodaj pliki projektów.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Color kolor = ustawienia.czytaj();
+            ekran.Fill = new SolidColorBrush(kolor);
+            R.Value = kolor.R; 
+            G.Value = kolor.G;
+            B.Value = kolor.B;
+            wyświetle_wartości();
+            okno.WindowStartupLocation = WindowStartupLocation.Manual;
+            Point p = ustawienia.odczytaj_pozycje();
+            okno.Top = p.X;
+            okno.Left = p.Y;
+
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Color kolor = new Color()
+            {
+                A = 255, 
+                G = (byte)this.G.Value,
+                B = (byte)this.B.Value,
+                R = (byte)this.R.Value };
+            (ekran.Fill as SolidColorBrush).Color = kolor;
+            wyświetle_wartości();
+        }
+       void  wyświetle_wartości()
+        {
+            TB.Text = ((int)this.B.Value).ToString();
+            TG.Text = ((int)this.G.Value).ToString();
+            TR.Text = ((int)this.R.Value).ToString();
+        }
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ustawienia.zapisz((ekran.Fill as SolidColorBrush).Color);
+            Point p= new Point(){ X=okno.Top,Y=okno.Left};
+            ustawienia.zapisz_pozycje(p);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                Close();
+        }
+    }
+}
---
+++ WpfApp1/ustawienia.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    class ustawienia
+    {
+       public static Color  czytaj()
+        {
+            Properties.Settings ustwienia = Properties.Settings.Default;
+            Color kolor = new Color()
+            {
+                A = ustwienia.A,
+                B = ustwienia.B,
+                G = ustwienia.G,
+                R = ustwienia.R
+            };
+            return kolor;
+        }
+        public  static Point odczytaj_pozycje()
+        {
+            Properties.Settings ustwienia = Properties.Settings.Default;
+
+            Point point = new Point() { X=ustwienia.X,Y=ustwienia.Y};
+            return point;
+        }
+        public static void zapisz_pozycje(Point p)
+        {
+            Properties.Settings ustwienia = Properties.Settings.Default;
+            ustwienia.X = p.X;
+            ustwienia.Y = p.Y;
+            ustwienia.Save();
+        }
+
+        public static void zapisz(Color kolor)
+        {
+            Properties.Settings ustawienia = Properties.Settings.Default;
+            ustawienia.A = kolor.A;
+            ustawienia.B = kolor.B;
+            ustawienia.G = kolor.G;
+            ustawienia.R = kolor.R;
+            ustawienia.Save();
+        }
+    }
+}
